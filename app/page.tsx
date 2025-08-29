31b92d3a4e42491529e03e28504f5b65cd23ff22
ignasxv <ignasxv@gmail.com>
2025-08-29 11:44:54 -0400

    fix: removed the admin link on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import WordleGameWithAuth from "@/components/wordle-game-with-auth"
-import { AdminLink } from "@/components/admin-link"
 import { initializeDatabase } from "@/lib/db"
 
 export default async function CWRUWordle() {
@@ -8,7 +7,6 @@ export default async function CWRUWordle() {
   
   return (
     <>
-      <AdminLink />
       <WordleGameWithAuth title="CWRU WORDLE" subtitle="Guess the CWRU word in 6 tries!" />
     </>
   )
