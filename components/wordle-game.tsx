b0ba391eee218e890d15ceeaa3951bad2b953d68
ignasxv <ignasxv@gmail.com>
2025-09-02 09:31:50 -0400

    ui: enter on the left-most, backspace on the rightmost

--- components/wordle-game.tsx
+++ components/wordle-game.tsx
@@ -434,7 +434,7 @@ export default function WordleGame({ userId }: WordleGameProps) {
     const rows = [
       ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
       ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-      ["Z", "X", "C", "V", "B", "N", "M", "BACKSPACE", "ENTER"],
+      ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"],
     ]
 
     return rows.map((row, i) => (
